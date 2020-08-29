a03add8ed19a47bfaf6226f3ed0763ba13da1cf8
Andrei Aquino <andreimartinbaquino@gmail.com>
2020-08-29 19:18:42 +0800

    initial commit connecting PHP and MySQL

---
+++ activity.php
@@ -0,0 +1,106 @@
+<?php include('dbcontrol.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Activity PHP CRUD</title>
+</head>
+<body>
+  <h1>CRUP PHP AND MySQL</h1><br>
+  <form method="POST" action="dbcontrol.php">
+    <label for="">First Name:</label>
+    <input type="text" name="firstname" id=""><br>
+    <label for="">Last Name:</label>
+    <input type="text" name="lastname" id=""><br>
+    <label for="">Email:</label>
+    <input type="email" name="email" id=""><br>
+    <label for="">City:</label>
+    <input type="text" name="city" id=""><br><br>
+    <input type="submit" name="save">
+  </form>
+
+  <br>
+  <div>
+    <?php
+      if($result->num_rows > 0) {
+        while($row = mysqli_fetch_array($result)) {
+          echo $row["id"] . " " . $row["firstname"] . " " . $row["lastname"] . " " . $row["email"] . " " . $row["city"] ."<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+    ?>
+  </div>
+  
+  <table>
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      <th>City</th>
+      <th colspan="2">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = mysqli_fetch_array($result)) { ?>
+      <tr>
+        <td><?php echo $row['id'] ?></td>
+        <td><?php echo $row['firstname'] ?></td>
+        <td><?php echo $row['lastname'] ?></td>
+        <td><?php echo $row['email'] ?></td>
+        <td><?php echo $row['city'] ?></td>
+        <td>
+          <a href="#">Update</a>
+        </td>
+        <td>
+          <a href="#">Delete</a>
+        </td>
+      </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+
+  <?php
+    // $servername = 'localhost';
+    // $username = 'root';
+    // $password = '';
+
+    // $connect = new mysqli($servername, $username, $password);
+
+    // if($connect->connect_error) {
+    //   die("Connection Failed ". $connect->connect_error);
+    // }
+
+    // $usedb = "use crud_account";
+    // $sql = "create database crud_account";
+    // if($connect->query($usedb) === true) {
+    //   echo "Successfully used crud_account db";
+    // } elseif ($connect->query($sql) === true) {
+    //   echo "Database created successfully";
+    // } else {
+    //   echo "Error creating database: " . $connect->error;
+    // }
+
+    // $createTableQuery = "
+    //   create table account(
+    //     id int(6) auto_increment primary key,
+    //     firstname varchar(30) not null,
+    //     lastname varchar(30) not null,
+    //     email varchar(50) not null,
+    //     city varchar(50) not null,
+    //     reg_date timestamp default current_timestamp on update current_timestamp
+    //   )
+    // ";
+
+    // crate table query
+    // if($connect->query($createTableQuery) === true) {
+    //   echo "Table created successfully!";
+    // } else {
+    //   echo "Error creating table: " . $connect->error;
+    // }
+  ?>
+</body>
+</html>
---
+++ dbcontrol.php
@@ -0,0 +1,54 @@
+<?php
+  $servername = 'localhost';
+  $username = 'root';
+  $password = '';
+  $dbname = 'crud_account';
+  $firstname = '';
+  $lastname = '';
+  $email = '';
+  $city = '';
+
+  // connect to mysql database
+  $connect = new mysqli($servername, $username, $password, $dbname);
+
+  if($connect->connect_error) {
+    die("Connection Failed" . $con->connect_error);
+  }
+
+  $stmt = $connect->prepare('
+    insert into account (firstname, lastname, email, city) values (?, ?, ?, ?)
+  ');
+
+  // when save button is clicked
+  if(isset($_POST['save'])) {
+    $firstname = test_input($_POST['firstname']);
+    $lastname = test_input($_POST['lastname']);
+    $email = test_input($_POST['email']);
+    $city = test_input($_POST['city']);
+
+    entertext($firstname, $lastname, $email, $city);
+    header('location: activity.php');
+  }
+
+  // get records
+  $sqlget = "select id, firstname, lastname, email, city from account";
+  $result = $connect->query($sqlget);
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  function entertext($a, $b, $c, $d) {
+    global $stmt;
+    $stmt->bind_param("ssss", $firstname, $lastname, $email, $city);
+    $firstname = $a;
+    $lastname = $b;
+    $email = $c;
+    $city = $d;
+    $stmt->execute();
+  }
+
+?>
